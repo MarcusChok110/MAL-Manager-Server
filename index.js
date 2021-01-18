e90708588b2aa52b5b9670caf5c17c156bb9a92a
Marcus Chok <marcus.chok110@gmail.com>
2021-01-17 23:24:20 -0500

    Add helmet as middleware and animelist and user routes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const helmet = require('helmet');
 
 require('dotenv').config();
 
 // routes
 const session = require('./routes/session');
+const user = require('./routes/user');
+const animelist = require('./routes/animelist');
 
 const PORT = process.env.PORT || 8888;
 const app = express();
@@ -18,8 +21,13 @@ app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// other middleware
+app.use(helmet());
+
 // Routes
 app.use('/session', session);
+app.use('/user', user);
+app.use('/animelist', animelist);
 
 app.get('/', (req, res) => {});
 
