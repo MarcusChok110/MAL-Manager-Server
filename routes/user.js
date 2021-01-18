3d337612c6805a94c9490006b28b44237b66f4d1
Marcus Chok <marcus.chok110@gmail.com>
2021-01-17 23:22:04 -0500

    Add user route to fetch user data from MAL API

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,24 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-const token = 'TOKEN_HERE';
+router.get('/', (req, res) => {
+  const token = req.header('Authorization');
+  const url =
+    'https://api.myanimelist.net/v2/users/@me?fields=anime_statistics';
+  const options = {
+    headers: {
+      Authorization: token,
+    },
+  };
 
-axios({
-  url: 'https://api.myanimelist.net/v2/users/@me',
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
-}).then((response) => {
-  console.log(response);
+  axios
+    .get(url, options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((response) => {
+      console.error(response);
+    });
 });
+
+module.exports = router;
