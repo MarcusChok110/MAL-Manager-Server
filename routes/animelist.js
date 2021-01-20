4e57aff10b42f332e152381991f6d7add2bc4851
Marcus Chok <marcus.chok110@gmail.com>
2021-01-20 18:49:12 -0500

    Add PUT route to update anime list entries

--- routes/animelist.js
+++ routes/animelist.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const { response } = require('express');
+const querystring = require('querystring');
 
 // get request to get anime list from myanimelist api
 router.get('/', (req, res) => {
@@ -25,6 +27,21 @@ router.get('/', (req, res) => {
 
 router.put('/:animeId', (req, res) => {
   const anime = req.params.animeId;
+  const url = `https://api.myanimelist.net/v2/anime/${anime}/my_list_status`;
+  const token = req.signedCookies.auth_token;
+  const options = { headers: { Authorization: `Bearer ${token}` } };
+  const data = querystring.stringify(req.body);
+
+  axios
+    .put(url, data, options)
+    .then((response) => {
+      res.json({ success: true });
+    })
+    .catch((response) => {
+      console.error(response);
+      res.json({ success: false });
+      throw new Error('Something went wrong with updating list entry');
+    });
 });
 
 router.delete('/:animeId', (req, res) => {
